e5b9474f13f5225da0a2a70c024ee4a8cc77150f
jizhuozhi <jizhuozhi.george@gmail.com>
2025-04-26 12:51:54 +0800

    test: use mock for stable testing

--- go.mod
+++ go.mod
@@ -2,10 +2,19 @@ module github.com/jizhuozhi/go-concurrency
 
 go 1.18
 
-require github.com/stretchr/testify v1.10.0
+require (
+	github.com/bytedance/mockey v1.2.14
+	github.com/stretchr/testify v1.10.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gopherjs/gopherjs v1.12.80 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/smartystreets/assertions v1.2.0 // indirect
+	github.com/smartystreets/goconvey v1.7.2 // indirect
+	golang.org/x/arch v0.11.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- ratelimiter/token_bucket_test.go
+++ ratelimiter/token_bucket_test.go
@@ -4,37 +4,47 @@ import (
 	"testing"
 	"time"
 
+	. "github.com/bytedance/mockey"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTokenBucketRateLimiter_Allow(t *testing.T) {
-	rate := int64(10)
-	capacity := int64(20)
-	ratelimiter := NewTokenBucketRateLimiter(rate, capacity)
+	PatchConvey("TestTokenBucketRateLimiter_Allow", t, func() {
+		origin := time.Now
+		base := time.Now().Round(time.Second)
+		Mock(time.Now).To(func() time.Time {
+			return base
+		}).Origin(&origin).Build()
 
-	// allow burst requests with capacity
-	for i := int64(0); i < capacity; i++ {
-		assert.True(t, ratelimiter.Allow())
-	}
-	for i := int64(0); i < capacity; i++ {
-		assert.False(t, ratelimiter.Allow())
-	}
+		assert.Equal(t, base, time.Now())
+		rate := int64(10)
+		capacity := int64(20)
+		ratelimiter := NewTokenBucketRateLimiter(rate, capacity)
 
-	// allow requests with rate
-	time.Sleep(1 * time.Second)
-	for i := int64(0); i < rate; i++ {
-		assert.True(t, ratelimiter.Allow())
-	}
-	for i := int64(0); i < capacity; i++ {
-		assert.False(t, ratelimiter.Allow())
-	}
+		// allow burst requests with capacity
+		for i := int64(0); i < capacity; i++ {
+			assert.True(t, ratelimiter.Allow())
+		}
+		for i := int64(0); i < capacity; i++ {
+			assert.False(t, ratelimiter.Allow())
+		}
 
-	// allow requests with at most capacity
-	time.Sleep(time.Duration(capacity/rate*2) * time.Second)
-	for i := int64(0); i < capacity; i++ {
-		assert.True(t, ratelimiter.Allow())
-	}
-	for i := int64(0); i < capacity; i++ {
-		assert.False(t, ratelimiter.Allow())
-	}
+		// allow requests with rate
+		base = base.Add(1 * time.Second)
+		for i := int64(0); i < rate; i++ {
+			assert.True(t, ratelimiter.Allow())
+		}
+		for i := int64(0); i < capacity; i++ {
+			assert.False(t, ratelimiter.Allow())
+		}
+
+		// allow requests with at most capacity
+		base = base.Add(time.Duration(capacity/rate*2) * time.Second)
+		for i := int64(0); i < capacity; i++ {
+			assert.True(t, ratelimiter.Allow())
+		}
+		for i := int64(0); i < capacity; i++ {
+			assert.False(t, ratelimiter.Allow())
+		}
+	})
 }
